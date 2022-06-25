a2e8c07388cc7cdc04e6368183b7fa57e297ffb9
kea6t <keasay26@gmail.com>
2022-06-25 17:31:27 -0400

    I will continue working on this hw once I am caught up with the lectures

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,105 @@
 // Global variables
 var score = 0;
 var currIndex = 0;
-var penaltyTime = 15;
-var totalTime = 75;
-var timerEl = document.getElementById("time")
+var penaltyTime = 10;
+var timeLeft = 75;
+var timerEl = document.getElementById("timer");
+var questionEl = document.getElementById("question-title");
+var displayAnswers = document.getElementById("answer-list")
+var questTitle = document.getElementById(".quest-title");
+
 
 var buttonEl = {
-  btnStartEl: document.querySelector("#startQuiz"),
-  btnChoicesEl: document.querySelector("#answer-list"),
-  btnSubmitEl: document.querySelector("#submit-quiz"),
-  btnBackEl: document.querySelector("#return-back"),
-  btnClearEl: document.querySelector("#clear-score")
+  startEl: document.querySelector("#start-quiz"),
+  choicesEl: document.querySelector("#answer-list"),
+  submitEl: document.querySelector("#submit-quiz"),
+  backEl: document.querySelector("#return-back"),
+  clearEl: document.querySelector("#clear-score")
 };
 
+// quiz questions with the answer set and passing the value in array
+var quizQuestions = [
+  {
+    question: "Commonly used data types DO NOT include: ",
+    answers: {
+      1: "Strings",
+      2: "Booleans",
+      3: "Alerts",
+      4: "Numbers"
+    },
+    correctAnswer: '3'
+  },
 
+  {
+    question: "The condition in an if / else statement is enclosed with _____________. ",
+    answer: {
+      1: "Quotes",
+      2: "Curly brackets",
+      3: "Parenthesis",
+      4: "Square brackets"
+    },
+    correctAnswer: '3' 
+  },
+
+  {
+    question: "Arrays in Javascript can be used to store ________________. ",
+    answer: {
+      1: "Numbers and Strings", 
+      2: "Other arrays",
+      3: "Booleans",
+      4: "All of the above"
+    },
+    correctAnswer: '4'
+
+  },
+
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answer: {
+      1: "JavaScript",
+      2: "Terminal / bash",
+      3: "For loops",
+      4: "console.log"
+    },
+    correctAnswer: '4'
+  }
+];
 
 
 var pageContentEl = document.querySelector("#page-content");
 
-// create array to hold tasks for saving
-var tasks = [];
 
 
-
-var startQuiz = function() {
-  timerEl.textContent = totalTime;
-  counter = setInterval(timer, 1000);
-
+// Timer that counts down from 75
+var countdown = function() {
+  var timeInterval = setInterval(function () {
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+    }else {
+      timerEl.innerHTML = timeLeft;
+      timeLeft--;
+    }
+  }, 1000);
 }
 
-var timer = function() {
-  if (totalTime <= 0) {
-    clearTimer();
-  } else {
-    totalTime--;
+
+var displayQuiz = function() {
+  // get the quiz
+  var quizSection = document.getElementById("quiz-section");
+  // get the value of the quiz display property
+  var displaySetting = quizSection.style.display;
+  // now toggle the quiz when the button is clicked, depending on current state
+  if (displaySetting == "none") {
+    // quiz is not visible then show it
+    quizSection.style.display = "block";
+  }else {
+    // quiz is visible then hide it
+    quizSection.style.display = "none";
   }
+  console.log(displayQuiz);
 }
+
 
 
 // Create a new task
-btnStartEl.addEventListener("click", startQuiz);
+buttonEl.startEl.addEventListener("click", countdown);
